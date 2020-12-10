cb3e6856209e2ddaadd0340799928d0838fe4a70
Alex <alexfrancisaaron@gmail.com>
2020-12-10 13:24:21 -0600

    commit tree bbe6ee1b9dce7c459104e3c39975e7390f39bbc8

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(x)
+    if x <= 1
+        return false
+    end
+    
+    i = 1
+    count = 0
+
+    while i <= x
+        if x % i == 0
+        count += 1
+        end
+        i += 1
+    end
+    if count > 2
+        return false
+    else
+        return true
+    end
+end
